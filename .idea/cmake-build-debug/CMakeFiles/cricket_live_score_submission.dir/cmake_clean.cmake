37d4234887c73bea6e576518ec6f68155d1c6f30
kavishka ganewattha <kaviya1234z@gmail.com>
2021-10-12 21:05:06 +0530

    file handling added

---
+++ .idea/cmake-build-debug/CMakeFiles/cricket_live_score_submission.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cricket_live_score_submission.dir/main.c.obj"
+  "cricket_live_score_submission.exe"
+  "cricket_live_score_submission.exe.manifest"
+  "cricket_live_score_submission.pdb"
+  "libcricket_live_score_submission.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cricket_live_score_submission.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
